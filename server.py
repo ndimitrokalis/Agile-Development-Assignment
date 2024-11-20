ff3a6fbf316d8f441a73f19811aa6eb3f6b361e3
ndimitrokalis <ndimitrokalis@nyc.gr>
2024-11-20 19:12:51 +0200

    Added login logic

--- server.py
+++ server.py
@@ -8,24 +8,40 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = None
+        if user is None:
+            return redirect('/login')
+        else:
+            if user.password == password:
+                login_user(user)
+                return redirect('/profile')
+            else:
+                return redirect('/login')
+    else:
+        return render_template('login.html')
 
 @app.route("/register")
 def register():
     return render_template('register.html')
 
 @app.route("/profile")
+@login_required
 def profile():
     return render_template('profile.html')
 
 @app.route("/dashboard")
+@login_required
 def dashboard():
     return render_template('dashboard.html')
 
 @app.route("/logout")
 def logout():
+    logout_user()
     return redirect('/')
 
 if __name__ == '__main__':
