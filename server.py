0f525224c02600a22d1a71a7c7a1129444ded330
annannkl <atzavara@nyc.gr>
2024-11-21 02:02:59 +0200

    This is a first approach for the register function.

--- server.py
+++ server.py
@@ -25,8 +25,17 @@ def login():
     else:
         return render_template('login.html')
 
-@app.route("/register")
-def register():
+@app.route("/register",methods=['GET', 'POST'])
+def register_user():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = None
+        try:
+            pass
+            return redirect('/login')
+        except:
+            return redirect('/register')
     return render_template('register.html')
 
 @app.route("/profile")
@@ -43,6 +52,10 @@ def dashboard():
 def logout():
     logout_user()
     return redirect('/')
+
+@LoginManager.unauthorized_handler
+def unauthorized_callback():
+    return redirect('/login')
 
 if __name__ == '__main__':
     app.run()
